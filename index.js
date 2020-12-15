3517c000527d7580bfe6a0611de5fd4aacf32e04
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2020-12-15 17:19:16 +0100

    incluimos clase points

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-class League {
+class League { // Creamos la clase liga.
 
-    constructor(name, teams=[], rounds=1){
+    constructor(name, teams=[], rounds=1){ //Constructor con sus parametros;
         this.name = name;
         this.teams = teams;
         this.rounds = rounds;
         this.matchDaySchedule = [];
     }
 }
-const NFLTeams = ["Minnesota Vikings", "Washington Football Team"]
-const nfl = new League("NFL", NFLTeams);
+
+class PointsBasedLeague extends nfl {
+    constructor (name, teams=[], rounds=1, pointsPerWin=3, pointsPerDraw=1, pointsPerLose=0){
+        super(name, teams, rounds);
+        this.pointsPerWin = pointsPerWin;
+        this.pointsPerDraw = pointsPerDraw;
+        this.pointsPerLose = pointsPerLose;
+    }
+}
+const NFLTeams = ["Minnesota Vikings", "Washington Football Team"] // Una Const con el nombre de los equipos.
+const nfl = new PointsBasedLeague("NFL", NFLTeams); // Otra consts con el nombre de la liga. 
+
 
 for (const team of nfl.teams){
     console.log(team);
