575daab1e18abedc7e6611cea20180d8b43502f2
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2020-12-15 17:52:56 +0100

    Hacemos la configuración de la liga

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 class League { // Creamos la clase liga.
 
-    constructor(name, teams=[], rounds=1){ //Constructor con sus parametros;
+    constructor(name, teams=[], config={}){ //Constructor con sus parametros;
         this.name = name;
         this.teams = teams;
-        this.rounds = rounds;
         this.matchDaySchedule = [];
+
+       
+        this.setup(config);
+        
+    }
+    setup(config){
+        const defaultConfig = { rounds: 1 }
+        this.config = Object.assign(this.defaultConfig, config);
+        
     }
 }
 
-class PointsBasedLeague extends nfl {
-    constructor (name, teams=[], rounds=1, pointsPerWin=3, pointsPerDraw=1, pointsPerLose=0){
-        super(name, teams, rounds);
-        this.pointsPerWin = pointsPerWin;
-        this.pointsPerDraw = pointsPerDraw;
-        this.pointsPerLose = pointsPerLose;
+
+class PointsBasedLeague extends League {
+    constructor (name, teams=[], config={}){
+        super(name, teams, config);
+    }
+    
+    setup(config){
+        const defaultConfig = {
+            rounds: 1,
+            pointsPerWin: 3,
+            pointsPerDraw: 1,
+            pointsPerLose: 0
+        }
+        this.config = Object.assign(defaultConfig, config)
     }
 }
 const NFLTeams = ["Minnesota Vikings", "Washington Football Team"] // Una Const con el nombre de los equipos.
-const nfl = new PointsBasedLeague("NFL", NFLTeams); // Otra consts con el nombre de la liga. 
-
+const config = { rounds: 2, pointsPerWin: 3 };
+const nfl = new PointsBasedLeague("NFL", NFLTeams, config); // Otra consts con el nombre de la liga. 
+console.log("CONFIG", nfl.config)
 
 for (const team of nfl.teams){
     console.log(team);
