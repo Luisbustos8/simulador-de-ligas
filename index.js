11897511da17bcac6a65d5e2a987bc97789c6978
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2020-12-15 16:53:30 +0100

    Creando clase liga

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+class League {
+
+    constructor(name, teams=[], rounds=1){
+        this.name = name;
+        this.teams = teams;
+        this.rounds = rounds;
+        this.matchDaySchedule = [];
+    }
+}
+
+const nfl = new League("NFL");
+console.log(nfl);
