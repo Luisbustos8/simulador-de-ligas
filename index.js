4097c0ecee4d1939f367febf3628575dfaf61f1e
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2020-12-16 13:00:19 +0100

    Usamos los metodos map y forEach para pintar los equipos por pantalla

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import nflLeague from "./classes/PointsBasedLeague.js"
 import { nflTeams } from "./teams.js"
 const config = { rounds: 2, pointsPerWin: 3 };
 const nfl = new nflLeague("NFL", nflTeams, config); // Otra consts con el nombre de la liga. 
-console.log("CONFIG", nfl.config)
 
-for (const team of nfl.teams){
-    console.log(team);
-}
+
+const teamNames = nfl.teams.map(team => team.name)
+
+teamNames.forEach(function(equipo){
+    console.log(equipo)
+})
